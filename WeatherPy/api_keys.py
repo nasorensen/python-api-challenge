202cdbad10f6e1ef8f767e2c7c5d5ddc80ac961d
nasorensen <nasorensen42@gmail.com>
2025-02-04 20:00:58 -0600

    Uploading completed notebooks

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "bc4f635b0faaaee4804197c807e81da5"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "439a653cef784d11a124376f0de6997e"
